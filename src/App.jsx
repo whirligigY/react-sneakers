5bf7be74c0e5dd62cc3a8cf386d1034742ae3dfd
Yulia <yulia@bigmac.local>
2022-04-21 17:56:30 +0700

    Fix modification; add main page search

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ function App() {
     <div className="wrapper">
       <header className="d-flex justify-content-between p-5">
         <div className="d-flex">
-          <img src="/img/logo.png" width="40" height="40" />
+          <img src="/img/logo.png" width="40" height="40" alt="logo"/>
           <div className="headerInfo ms-3">
             <h3 className="mb-0">React Sneakers</h3>
             <p className="m-0">The Best sneakers shop</p>
@@ -17,26 +17,33 @@ function App() {
               src="/img/basket.svg"
               width="18"
               height="18"
+              alt="basket"
             />
             <span>1 005$</span>
           </li>
           <li>
-            <img src="/img/user.svg" width="20" height="20" />
+            <img src="/img/user.svg" width="20" height="20" alt="user"/>
           </li>
         </ul>
       </header>
       <div className="content p-5">
-        <h1>Sneakers</h1>
+        <div className="d-flex justify-content-between">
+          <h1>Sneakers</h1>
+          <div className="search-block">
+            <img src="/img/plus.svg" alt="plus"/>
+            <input placeholder="Search..." type="search"/>
+          </div>
+          </div>
         <div className="card-container d-flex justify-content-left">
           <div className="card d-flex flex-column  align-items-center">
             <img
               width="133"
               height="112"
               src="/img/sneakers/1.jpg"
-              alt=""
+              alt="sneakers"
             ></img>
             <p className="card-text">Men's sneakers Nike Blazer Mid Suede</p>
-            <div className="d-flex justify-content-between">
+            <div className="card-info d-flex justify-content-between">
               <div className="d-flex flex-column">
                 <span className="card-subtext">price:</span>
                 <b>110 $</b>
@@ -51,10 +58,10 @@ function App() {
               width="133"
               height="112"
               src="/img/sneakers/2.jpg"
-              alt=""
+              alt="sneakers"
             ></img>
             <p className="card-text">Men's sneakers Nike Air Max 270</p>
-            <div className="d-flex justify-content-between">
+            <div className="card-info d-flex justify-content-between">
               <div className="d-flex flex-column">
                 <span className="card-subtext">price:</span>
                 <b>112 $</b>
@@ -72,7 +79,7 @@ function App() {
               alt="sneakers"
             ></img>
             <p className="card-text">Men's sneakers Nike Blazer Mid Suede</p>
-            <div className="d-flex justify-content-between">
+            <div className="card-info d-flex justify-content-between">
               <div className="d-flex flex-column">
                 <span className="card-subtext">price:</span>
                 <b>111 $</b>
@@ -90,7 +97,7 @@ function App() {
               alt="sneakers"
             ></img>
             <p className="card-text">Men's sneakers Nike Blazer Mid Suede</p>
-            <div className="d-flex justify-content-between">
+            <div className="card-info d-flex justify-content-between">
               <div className="d-flex flex-column">
                 <span className="card-subtext">price:</span>
                 <b>110 $</b>
